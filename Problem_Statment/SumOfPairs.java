cd2ab95dff5dcc2467533ef7d0b542bc69b2de3a
CodeWithRushi <73602443+CodeWithRushi@users.noreply.github.com>
2021-04-02 20:09:29 +0530

    Create SumOfPairs.java

---
+++ Problem_Statment/SumOfPairs.java
@@ -0,0 +1,65 @@
+/*
+WAP A PROGRAM FOR THIS PROBLEM AND PRINT THE TOTAL NUMBERS OF PAIRS WHOSE SUM IS K.
+PRINT "0" IF NO PAIR IS FOUND
+ * */
+package Problem_Statment;
+import java.util.*;
+public class SumOfPairs {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("How Many Number Do You Want To Insert");
+		int n=sc.nextInt();
+		System.out.println("Enter Sum Value");
+		int k=sc.nextInt();
+		int pair=0;
+		int arr[]=new int[n];
+		System.out.println("Enter Element");
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				
+				if((arr[i]+arr[j])==k)
+				{
+					
+					pair++;
+					System.out.println(arr[i]+","+arr[j]);
+				}
+				
+				
+			}
+		}
+
+		if(pair==0)
+		{
+			System.out.println("0");
+		}
+		else
+		{
+			System.out.println(pair);
+		}
+	}
+
+}
+
+/*
+How Many Number Do You Want To Insert
+5
+Enter Sum Value
+6
+Enter Element
+5
+1
+2
+3
+4
+5,1
+2,4
+2
+
+ */
