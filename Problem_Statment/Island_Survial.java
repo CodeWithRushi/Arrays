5dd2135b0292dd0688dde2ccc3cc03e436f557f1
CodeWithRushi <73602443+CodeWithRushi@users.noreply.github.com>
2021-04-21 20:44:40 +0530

    Update Island_Survial.java

--- Problem_Statment/Island_Survial.java
+++ Problem_Statment/Island_Survial.java
@@ -1,4 +1,15 @@
-package Problem_Statment;
+/* 	Wap to Check if it is possible to survive on Island
+
+You are a poor person in an island. There is only one shop in this island, this shop is open on all days of the week except for Sunday. Consider following constraints: 
+
+N – Maximum unit of food you can buy each day.
+S – Number of days you are required to survive.
+M – Unit of food required each day to survive.
+Currently, it’s Monday, and you need to survive for the next S days. 
+Find the minimum number of days on which you need to buy food from the shop so that you can survive the next S days, or determine that it isn’t possible to survive. 
+
+*/
+
 import java.util.*;
 public class Island_Survial {
 
